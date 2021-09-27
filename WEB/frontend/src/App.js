fd82b9f4a7468ceb0798c94ad4a64ce7cbaf2a0e
Chan <qufstar46@gmail.com>
2021-09-27 13:38:47 +0000

    feat: apply code formatters

--- WEB/frontend/src/App.js
+++ WEB/frontend/src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
-import MyGroup from './MyGroup'
-import StudyGroupOutline from './StudyGroupOutline'
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
+import MyGroup from './MyGroup';
+import StudyGroupOutline from './StudyGroupOutline';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function App() {
+  const a=1;
   return (
-    <div><MyGroup/></div>
+    <div>
+      <MyGroup />
+    </div>
   );
 }
 
--- WEB/frontend/src/MyMenu.js
+++ WEB/frontend/src/MyMenu.js
@@ -1,5 +1,6 @@
 import { Typography } from "@mui/material"
 
+const a=1;
 const MyMenu = () => {
  return <><Typography variant="body1" component="h1">마이 메뉴</Typography></>   
 }
