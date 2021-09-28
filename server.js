caac977704cc61f3853687d9c79724b2524723d6
y00nZZang <84168285+y00nZZang@users.noreply.github.com>
2021-09-28 14:37:08 +0900

    eslint prerrier 적용

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
 
 dotenv.config();
 
 const app = express();
-app.set('port', process.env.PORT || 3001);
+app.set('port', process.env.PORT || 5000);
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+
+/* req.body 검증용
+app.use((req,res,next)=>{
+	console.log(req.body);
+	next();
+});
+*/
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('DB 연결 성공'))
+  .catch(e => console.log('MongoDB error: ', e));
+
 
 app.listen(app.get('port'), ()=>{
   console.log(app.get('port'), '번 포트 서버 구동중');
 })
+
