db816d8220c0dad968bb3659e824ba6d1ce117d5
Chan <qufstar46@gmail.com>
2021-10-05 11:19:38 +0000

    fix: 가운데 위치로 옮기기 위한 CSS 추가

--- WEB/frontend/src/MyGroup.js
+++ WEB/frontend/src/MyGroup.js
@@ -1,6 +1,14 @@
-import MyStudyGroup from './MyStudyGroup'
-import MyMenu from './MyMenu'
+import { Box } from '@mui/material';
+import MyStudyGroup from './MyStudyGroup';
+import MyMenu from './MyMenu';
 
-const MyGroup = () => <><MyStudyGroup/><MyMenu/></>
+const MyGroup = () => (
+  <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+    <div>
+      <MyStudyGroup />
+      <MyMenu />
+    </div>
+  </Box>
+);
 
-export default MyGroup
+export default MyGroup;
