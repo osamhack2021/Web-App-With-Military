41290013499ae09f52b5d4ebdc13d2fa35897e38
SungjunIm <89720515+SungjunIm@users.noreply.github.com>
2021-10-11 14:43:16 +0900

    Update groupRoute.js

--- WEB/backend/newfeature/routes/groupRoute.js
+++ WEB/backend/newfeature/routes/groupRoute.js
@@ -23,6 +23,13 @@ groupRouter.get('/', async(req,res) => {
     
 })
 
+/*
+GET : localhost:3000/group을 통해 모든 group을 불러올 수 있다.
+그룹명은 필수 값이며 그 외의 것은 ./models/group을 통해 확인할 수 있다. 
+*/
+
+*/
+
 // 그룹 만들기 
 groupRouter.post('/', async(req,res) => {
     
@@ -40,6 +47,12 @@ groupRouter.post('/', async(req,res) => {
     }
     
 })
-
+/*
+POST : localhost:3000/group을 통해 그룹을 추가할 수 있다.
+Body(JSON)으로
+{
+    "name" : "abdStudy"
+} 으로 그룹을 추가할 수 있다.
+*/
 
 module.exports = {groupRouter};
