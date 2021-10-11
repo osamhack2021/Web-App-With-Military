f9245879d0270fb7e8b560dc0108fe0d7b838591
SungjunIm <89720515+SungjunIm@users.noreply.github.com>
2021-10-11 13:43:37 +0900

    Update server.js

--- WEB/backend/newfeature/server.js
+++ WEB/backend/newfeature/server.js
@@ -1,24 +1,38 @@
 var express = require("express");
 var mongoose = require("mongoose");
+var cookieParser = require("cookie-parser");
+var dotenv = require("dotenv");
 
-mongoose.connect("mongodb://localhost:27017/main");
+
+mongoose.connect("mongodb://localhost:27017/main3");
 
 var app = express();
 app.use(express.json());  //req.body 사용할 때 
 app.use(express.urlencoded({extends: true}));
+app.use(cookieParser());
+
 
 // Schema 연결
-var { User } = require("../../../feature/models/User");
-var { Board} = require("../../../feature/models/board");
+var { User } = require("./models/User");
+var { Board} = require("./models/board");
+var { group } = require("./models/group");
+
 
 // Router 연결
-var {boardRouter}  = require("../../../feature/routes/boardRoute");
-var {userRouter} = require("../../../feature/routes/userRoute");
-var {commentRouter} = require("../../../feature/routes/commentRoute");
+var {boardRouter}  = require("./routes/boardRoute");
+var {userRouter} = require("./routes/userRoute");
+var {commentRouter} = require("./routes/commentRoute");
+var {groupRouter} = require("./routes/groupRoute");
+var {groupBoardRouter} = require('./routes/groupBoardRoute');
+var {groupCommentRouter} = require('./routes/groupComment');
 
-app.use("/board", boardRouter);
+
+app.use("/board", boardRouter); 
 app.use("/user", userRouter);
 app.use("/board/:boardId/comment", commentRouter);
+app.use("/group", groupRouter);
+app.use("/groupBoard" , groupBoardRouter);
+app.use("/groupBoard/:groupId/comment" , groupCommentRouter);
 
 app.listen(3000, () => {
 
