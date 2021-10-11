d302932476107bf53df8f8077788d093d52bcdb9
SungjunIm <89720515+SungjunIm@users.noreply.github.com>
2021-10-11 15:06:44 +0900

    Update groupComment.js

--- WEB/backend/newfeature/routes/groupComment.js
+++ WEB/backend/newfeature/routes/groupComment.js
@@ -34,7 +34,7 @@ groupCommentRouter.post("/", async(req,res) => {
         var user = await User.findByIdAndUpdate(userId);
         if(!board || !user) return res.status(400).send({err: "board or user does not exist"});
         
-        var comment = new Comment({content, user});
+        var comment = new Comment({content, board, user});
         await comment.save();
         return res.send({comment});
 
@@ -45,6 +45,18 @@ groupCommentRouter.post("/", async(req,res) => {
     }
 
 });
+
+
+/* 
+POST : http://localhost:3000/groupBoard/:groupId/comment 를 통해 게시판에 댓글을 작성할 수 있다. 
+groupId 는 req.params(url)을 통해 받고 
+Body(JSON)으로 
+{
+    "content" : "좋아요",
+    "userId" "(objectId)"
+} 와 같다.
+-> db는 comments에 저장된다. 
+*/
 
 //댓글 조회
 groupCommentRouter.get("/", async(req,res) => {
@@ -58,5 +70,8 @@ groupCommentRouter.get("/", async(req,res) => {
 
 });
 
+/*
+GET : http://localhost:3000/board/:boardId/comment 을 통해 comments에 있는 db를 불러와 게시판에 작성된 댓글을 확인할 수 있다.
+*/
 
 module.exports = {groupCommentRouter};
