9f30543446e74c9cb9cd099e3fe0e57a347283f0
Seungho-Na <52454427+Seungho-Na@users.noreply.github.com>
2021-10-15 10:10:52 +0000

    fix: 로그인/회원가입 api수정

--- WEB/frontend/src/modules/userAuth.js
+++ WEB/frontend/src/modules/userAuth.js
@@ -1,28 +1,35 @@
 import { handleActions } from 'redux-actions';
 import axios from 'axios';
-import { login as loginApi, register as registerApi } from '../lib/api/userAuth';
-
+import {
+  login as loginApi,
+  register as registerApi,
+} from '../lib/api/userAuth';
 // 액션 타입 정의
-
 const LOGIN = 'auth/LOGIN';
 const REGISTER = 'auth/RESIGER';
-
 export function auth() {
-  const request = axios.get('/users/auth')
-    .then((response) => response.data);
+  const request = axios.get('/users/auth').then((response) => response.data);
   return {
     type: AUTH_USER,
     payload: request,
   };
 }
-
 // 액션 생성 함수 정의
-
 export const login = (formData) => async (dispatch) => {
   const res = await loginApi(formData);
-  
+  if (res.status !== 200) {
+    const data = {
+      isSuccessful: false,
+      loginFailure: res.data.loginFailure,
+    };
+    dispatch({
+      type: LOGIN,
+      payload: data,
+    });
+    return data;
+  }
   const data = {
-    success: true,
+    isSuccessful: true,
     userData: res.data.user,
   };
   dispatch({
@@ -31,18 +38,21 @@ export const login = (formData) => async (dispatch) => {
   });
   return data;
 };
-
 export const register = (formData) => async (dispatch) => {
   const res = await registerApi(formData);
   if (res.status !== 200) {
-    // 이렇게 쓰면 ESLint 에러나옴
-    // const data = {
-    //   success: false,
-    //   registerFailure: res.data.registerFailure,
-    // };
+    const data = {
+      isSuccessful: false,
+      registerFailure: res.data.registerFailure,
+    };
+    dispatch({
+      type: REGISTER,
+      payload: data,
+    });
+    return data;
   }
   const data = {
-    success: true,
+    isSuccessful: true,
     userData: res.data.user,
   };
   dispatch({
@@ -51,9 +61,7 @@ export const register = (formData) => async (dispatch) => {
   });
   return data;
 };
-
 // 초기상태
-
 const initialState = {
   userData: {
     id: null,
@@ -65,24 +73,16 @@ const initialState = {
     loginFailure: null,
   },
 };
-
 // 리듀서
-
-const userAuth = handleActions({
-  [LOGIN]: (state, { payload: { userData, loginFailure, success } }) => ({
-    ...state,
-    user: {
-      // ESLint rule : no-underscore-dangle
-      // id: userData._id,
-      userName: userData.userName,
-      // 추가 데이터
-    },
-    login: {
-      success,
-      loginFailure,
-    },
-  }),
-},
-initialState);
-
+const userAuth = handleActions(
+  {
+    [LOGIN]: (state, { payload: { success, userData } }) => ({
+      ...userData,
+      login: {
+        success,
+      },
+    }),
+  },
+  initialState,
+);
 export default userAuth;
--- WEB/frontend/src/pages/PCRegisterPage.jsx
+++ WEB/frontend/src/pages/PCRegisterPage.jsx
@@ -4,22 +4,19 @@
 import React, { useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import PCRegisterForm from '../components/pcregister/PCRegisterForm';
-import PCRegisterTemplate from '../components/pcregister/PCRegisterTemplate';
+import RegisterForm from '../components/register/RegisterForm';
+import RegisterTemplate from '../components/register/RegisterTemplate';
 import { register } from '../modules/userAuth';
-
-const PCRegisterPage = ({ history }) => {
-  // const dispatch = useDispatch();
+const RegisterPage = ({ history }) => {
+  const dispatch = useDispatch();
   // error 행
   const error = { email: false, userName: false };
-
   const [form, setForm] = useState({
     email: '',
     password: '',
     confirmPassword: '',
     userName: '',
   });
-
   const onChange = (e) => {
     const nextForm = {
       ...form,
@@ -28,35 +25,29 @@ const PCRegisterPage = ({ history }) => {
     setForm(nextForm);
     console.log(form);
   };
-
   const onSubmit = (e) => {
     e.preventDefault();
-
     if (form.password !== form.confirmPassword) {
       return alert('비밀번호가 일치하지 않습니다.');
     }
-
     const formData = {
       email: form.email,
       password: form.password,
       userName: form.userName,
     };
-
-  //   dispatch(register(formData))
-  //     .then((response) => {
-  //       if (response.success) {
-  //         history.push('/login');
-  //       } else {
-  //         alert('Failed to sign up');
-  //       }
-  //     });
+    dispatch(register(formData))
+      .then((response) => {
+        if (response.isSuccessful) {
+          history.push('/login');
+        } else {
+          alert('Failed to sign up');
+        }
+      });
   };
-
   return (
-    <PCRegisterTemplate>
-      <PCRegisterForm form={form} onChange={onChange} onSubmit={onSubmit} error={error} />
-    </PCRegisterTemplate>
+    <RegisterTemplate>
+      <RegisterForm form={form} onChange={onChange} onSubmit={onSubmit} error={error} />
+    </RegisterTemplate>
   );
 };
-
-export default withRouter(PCRegisterPage);
+export default withRouter(RegisterPage);
