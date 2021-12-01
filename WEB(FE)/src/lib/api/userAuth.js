d356373330c74586702bb24f1761caddc0d2f6ab
gonudayo <kgw010119@gmail.com>
2021-12-01 13:36:51 +0000

    init: 클라이언트 부분 초기화

--- WEB(FE)/src/lib/api/userAuth.js
+++
@@ -1,7 +0,0 @@
-import client from './client';
-
-export const login = ({ email, password }) => client.post('/users/login', { email, password });
-
-export const register = ({ email, password, userName }) => client.post('/users/register', { email, password, userName });
-
-export const logout = () => client.get('/users/logout');
