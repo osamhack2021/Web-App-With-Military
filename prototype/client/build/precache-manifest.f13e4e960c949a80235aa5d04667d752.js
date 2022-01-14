02286b9c768c641a8292a9a29b84e59699ef8755
Gunwoo Kim <kgw010119@gmail.com>
2022-01-14 14:21:21 +0000

    chore: Add React Build
    ec2에서는 메모리가 부족하여 빌드를 할 수 없어 커밋함

---
+++ prototype/client/build/precache-manifest.f13e4e960c949a80235aa5d04667d752.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aa6e5ab2a301d9559f51e6786fb1ad7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1a2cb7b5e62ab42c57f",
+    "url": "/static/css/2.3e3db28c.chunk.css"
+  },
+  {
+    "revision": "a306e2a29c7eb50f43a4",
+    "url": "/static/css/main.b3f4343f.chunk.css"
+  },
+  {
+    "revision": "f1a2cb7b5e62ab42c57f",
+    "url": "/static/js/2.78cc274d.chunk.js"
+  },
+  {
+    "revision": "f9d929b8100131c8194846360ebebd07",
+    "url": "/static/js/2.78cc274d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a306e2a29c7eb50f43a4",
+    "url": "/static/js/main.0b43a674.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  },
+  {
+    "revision": "1612a3b8d0b11b34f3c7057b001361f3",
+    "url": "/static/media/logo_white.1612a3b8.svg"
+  }
+]);
