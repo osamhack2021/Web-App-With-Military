9787cf458b91edd4ee3a343ee88525f2e5b3a09f
김건우 <kgw010119@gmail.com>
2022-01-27 14:18:57 +0000

    fix: ranking 정렬 방식 간소화

--- server/src/routes/ranking/ranking.ctrl.js
+++ server/src/routes/ranking/ranking.ctrl.js
@@ -4,89 +4,13 @@ const { Group } = require('../../models/Group');
 const get = {
   // 전체 유저 랭킹
   user: async (req, res) => {
-    const result = await User.aggregate([
-      {
-        $sort: {
-          totalTime: -1,
-        },
-      },
-      {
-        $group: {
-          // Add in an array
-          _id: null,
-          items: {
-            $push: '$$ROOT',
-          },
-        },
-      },
-      {
-        $unwind: {
-          // De-normalize and get index
-          path: '$items',
-          includeArrayIndex: 'items.rank',
-        },
-      },
-      {
-        $replaceRoot: {
-          // Reshape
-          newRoot: '$items',
-        },
-      },
-      {
-        $addFields: {
-          // Add 1 to get to proper rank as array is index starts 0
-          rank: {
-            $add: ['$rank', 1],
-          },
-          password: null,
-          token: null,
-          create: null,
-          email: null,
-        },
-      },
-    ]);
+	  const result = await User.find().sort({totalTime: -1});
     return res.status(200).json({ success: true, result});
   },
 
   // 전체 그룹 랭킹
   group: async (req, res) => {
-    const result = await Group.aggregate([
-      {
-        $sort: {
-          totalTime: -1,
-        },
-      },
-      {
-        $group: {
-          // Add in an array
-          _id: null,
-          items: {
-            $push: '$$ROOT',
-          },
-        },
-      },
-      {
-        $unwind: {
-          // De-normalize and get index
-          path: '$items',
-          includeArrayIndex: 'items.rank',
-        },
-      },
-      {
-        $replaceRoot: {
-          // Reshape
-          newRoot: '$items',
-        },
-      },
-      {
-        $addFields: {
-          // Add 1 to get to proper rank as array is index starts 0
-          rank: {
-            $add: ['$rank', 1],
-          },
-        },
-      },
-    ]);
+	  const result = await Group.find().sort({totalTime: -1});
     return res.status(200).json({ success: true, result});
   },
 };
