1a6c9b56d179df741e3956a77216ead006d71115
Seungho-Na <bulestat000@naver.com>
2022-01-31 10:52:37 +0000

    feature: mui Tab 랭킹 페이지 적용

--- client/src/components/views/RankingPage/RankingPage.js
+++ client/src/components/views/RankingPage/RankingPage.js
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from 'react';
+import { Box, Container, Grid, Typography, Tab, Tabs } from '@mui/material';
+import { Link } from 'react-router-dom';
 import Axios from 'axios';
 
-function RankingPage(props) {
+export default function RankingPage(props) {
 	const target = props.match.params.target;
 	const [Users, setUsers] = useState([]);
 	const [Groups, setGroups] = useState([]);
+	
+	const [value, setValue] = useState( target === 'user' ? 0 : 1 );
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
 	useEffect(() => {
 		Axios.get('/api/ranking/user').then((response) => {
@@ -23,56 +30,70 @@ function RankingPage(props) {
 		});
 	}, []);
 	
-	if(target === "user") {
-		return (
-		<div>
-			<h1>유저 랭킹</h1>
-			<br />
-			{Users.map((user, i) => (
-				<a href={`/users/${user._id}`}>
-					{i+1} 등 - {user.name} : {user.totalTime}
+	let result;
+	
+	switch (target) {
+		case "user":
+			result = 
+				<div>
 					<br />
-				</a>
-			))}
-		</div>
-	);
-	} else if(target === "group") {
-		return (
-		<div>
-			<h1>그룹 랭킹</h1>
-			<br />
-			{Groups.map((group, i) => (
-				<a href={`/groups/${group._id}`}>
-					{i+1} 등 - {group.groupName} : {group.totalTime}
+					{Users.map((user, i) => (
+						<a href={`/users/${user._id}`}>
+							{i+1} 등 - {user.name} : {user.totalTime}
+							<br />
+						</a>
+					))}
+				</div>
+			break;
+		case "group":
+			result = 
+				<div>
 					<br />
-				</a>
-			))}
-		</div>
-	);
-	} else {
-		return (
-		<div>
-			<h1>유저 랭킹</h1>
-			<br />
-			{Users.map((user, i) => (
-				<a href={`/users/${user._id}`}>
-					{i+1} 등 - {user.name} : {user.totalTime}
-					<br />
-				</a>
-			))}
-			<h1>그룹 랭킹</h1>
-			<br />
-			{Groups.map((group, i) => (
-				<a href={`/groups/${group._id}`}>
-					{i+1} 등 - {group.groupName} : {group.totalTime}
-					<br />
-				</a>
-			))}
-		</div>
-	);
+					{Groups.map((group, i) => (
+						<a href={`/groups/${group._id}`}>
+							{i+1} 등 - {group.groupName} : {group.totalTime}
+							<br />
+						</a>
+					))}
+				</div>
+			break;
+		default:
+			result = <div></div>
 	}
-
+	
+	return (
+		<Container 
+			component="main"
+			maxWidth="lg"
+			sx={{
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+			}}
+		>
+			<br/>
+			<Tabs
+				value={value}
+				onChange={handleChange}
+				aria-label="basic tabs example"
+				textColor="secondary"
+				indicatorColor="secondary"
+			>
+				<Tab
+					label="유저 랭킹"
+					sx={{ fontSize: '2rem', fontWeight: 'bold' }}
+					component={Link}
+					to={'/ranking/user'}
+				/>
+				<Tab
+					label="그룹 랭킹"
+					sx={{ fontSize: '2rem', fontWeight: 'bold' }}
+					component={Link}
+					to={'/ranking/group'}
+				/>
+			</Tabs>
+			{ result }
+		</Container>
+	);
 	
 }
-
-export default RankingPage;
