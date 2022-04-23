86525dba0ce3b6148c9d94fde2a7b879407dfef9
Seungho-Na <bulestat000@naver.com>
2022-04-23 14:28:07 +0000

    feat: 색상 재정의

--- client/src/components/commons/TierText.js
+++ client/src/components/commons/TierText.js
@@ -4,23 +4,29 @@ import { Box, Typography } from "@mui/material";
 export default function TierText({point, tier, variant}) {
   function matchColor(p) {
     if (p < 1000) {
+      //언랭크
+      return "#A19D94"
+    } else if(1000 <= p < 5000) {
       //브론즈
       return "#DA8736"
-    } else if(1000 <= p < 5000) {
+    } else if(5000 <= p < 20000) {
       //실버
       return "#A4A4A4"
-    } else if(5000 <= p < 15000) {
+    } else if(20000 <= p < 50000) {
       //골드
       return "#ECD351"
-    } else if(15000 <= p < 50000) {
+    } else if(50000 <= p < 100000) {
       //플레티넘
       return "#70D9AD"
-    } else if(15000 <= p < 50000) {
-      //플레티넘
-      return "#70D9AD"
-    } else {
+    } else if (100000 <= p < 200000) {
       //다이아
-      return "#AAAAAA"
+      return "#35D1C8"
+    } else if (200000 <= p < 500000) {
+      //마스터
+      return "#FF4B7A"
+    } else {
+      //슈프림
+      return "#24FFF2"
     }
   }
 
