0a62d50861fb2c9b903eb9fdb78f9091e6b39b6d
kimugonu <kgw010119@gmail.com>
2022-08-11 18:25:14 +0900

    fix: RankingPage detail

--- client/src/components/views/RankingPage/Sections/TopScoreCard.js
+++ client/src/components/views/RankingPage/Sections/TopScoreCard.js
@@ -1,37 +1,35 @@
-import React from 'react';
-import {Box, Card, CardContent, CardMedia, Typography} from '@mui/material';
-import { styled } from '@mui/material/styles';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Box, Card, CardContent, CardMedia, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { Link } from "react-router-dom";
+import TierText from "../../../commons/TierText";
 
 const RankNumberBox = styled(Box)({
   width: 35,
   height: 35,
-  position: 'absolute',
+  position: "absolute",
   left: 0,
-  backgroundColor: '#c4c4c4',
-  color: 'white',
-  borderRadius: '0 0px 3px 0',
-  fontWeight: 'bold',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-})
+  backgroundColor: "#c4c4c4",
+  color: "white",
+  borderRadius: "0 0px 3px 0",
+  fontWeight: "bold",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+});
 
 const StyledCard = styled(Card)(({ theme }) => ({
   backgroundColor: theme.palette.background.default,
-  display: 'flex',
-  position: 'relative',
-  width: '70%',
+  display: "flex",
+  position: "relative",
+  width: "70%",
   height: 150,
-  textDecoration: 'none',
+  textDecoration: "none",
 }));
 
-export default function TopScoreCard({data}) {
+export default function TopScoreCard({ data }) {
   return (
-    <StyledCard
-      component={Link}
-      to={data.link}
-    >
+    <StyledCard component={Link} to={data.link} elevation={12}>
       <RankNumberBox>{data.rank}</RankNumberBox>
       <CardMedia
         component="img"
@@ -40,29 +38,24 @@ export default function TopScoreCard({data}) {
         alt="group card"
       />
       <CardContent>
-        <Typography component={Box} variant="h5">
+        <Typography component={Box} variant="h4">
           {data.name}
         </Typography>
         <Typography
           component={Box}
           style={{
             background:
-            '-webkit-linear-gradient(180deg, #66CFA3 0%, rgba(123, 235, 188, 0.8) 100%)',
-            webkitBackgroundClip: 'text',
-            WebkitTextFillColor: 'transparent',
-            fontWeight: 'bold',
-            fontSize: '1.5rem',
+              "-webkit-linear-gradient(180deg, #66CFA3 0%, rgba(123, 235, 188, 0.8) 100%)",
+            webkitBackgroundClip: "text",
+            WebkitTextFillColor: "transparent",
+            fontWeight: "bold",
+            fontSize: "1.5rem",
           }}
         >
           {data.tier}
         </Typography>
-        <Typography
-          component={Box}
-        >
-          {data.score}
-        </Typography>
+        <Typography component={Box}>{data.score}</Typography>
       </CardContent>
     </StyledCard>
   );
-  
 }
--- client/src/components/views/RankingPage/Sections/WithIn5thScoreCard.js
+++ client/src/components/views/RankingPage/Sections/WithIn5thScoreCard.js
@@ -1,45 +1,42 @@
-import React from 'react';
-import {Avatar, Box, Paper, Typography} from '@mui/material';
-import { styled } from '@mui/material/styles';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Avatar, Box, Paper, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { Link } from "react-router-dom";
 
 const RankNumberBox = styled(Box)({
   width: 35,
   height: 35,
-  position: 'absolute',
+  position: "absolute",
   left: 0,
-  backgroundColor: '#c4c4c4',
-  color: 'white',
-  borderRadius: '0 0px 3px 0',
-  fontWeight: 'bold',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center'
-})
+  backgroundColor: "#c4c4c4",
+  color: "white",
+  borderRadius: "3px 0px 3px 0px",
+  fontWeight: "bold",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+});
 
 const StyledPaper = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.background.default,
   //width: 200,
   height: 180,
-  display: 'flex',
-  position: 'relative',
-  flexDirection: 'column',
-  alignItems: 'center',
-  textDecoration: 'none',
+  display: "flex",
+  position: "relative",
+  flexDirection: "column",
+  alignItems: "center",
+  textDecoration: "none",
 }));
 
-
-export default function TopScoreCard({data}) {
+export default function TopScoreCard({ data }) {
   return (
-    <StyledPaper
-      component={Link}
-      to={data.link}
-    >
+    <StyledPaper component={Link} to={data.link} elevation={12}>
       <RankNumberBox>{data.rank}</RankNumberBox>
-      <Avatar 
-        src={data.image}  
-      />
-      <Typography>{data.name}</Typography>
+      <br />
+      <Avatar src={data.image} />
+      <Typography>
+        <b>{data.name}</b>
+      </Typography>
       <Typography>{data.tier}</Typography>
       <Typography>{data.score}</Typography>
     </StyledPaper>
