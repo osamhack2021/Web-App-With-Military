c54adcf2292c76030e4bb0139e0d943d2a28774e
kimugonu <kgw010119@gmail.com>
2022-08-16 22:15:56 +0900

    comment: Board 스키마 주석 추가

--- server/src/models/Board/Board.js
+++ server/src/models/Board/Board.js
@@ -1,25 +1,31 @@
 const mongoose = require('mongoose');
 
 const boardSchema = mongoose.Schema({
+  // 제목
   title: {
     type: String,
     require: true,
   },
+  // 활동 시간
   elapsedTime: {
     type: Number,
     default: null,
   },
+  // 작성자 ID
   writerId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
   },
+  // 그룹 ID
   groupId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Group',
   },
+  // 작성 날짜
   posted: {
     type: Date,
   },
+  // 내용
   content: {
     type: String,
     require: true,
