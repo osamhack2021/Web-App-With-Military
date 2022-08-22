d6c4ecce7325e2df54862232ce6f1ffd45df9bef
Seungho-Na <https://github.com/Seungho-Na/Voca_React>
2022-08-23 00:11:51 +0900

    feature: 검색시 자동완성 기능 추가

--- client/src/components/views/Search/Sections/SearchBar.js
+++ client/src/components/views/Search/Sections/SearchBar.js
@@ -1,43 +1,64 @@
-import React, { useState } from 'react';
+import Axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import { styled } from '@mui/material/styles';
 import { Box, InputAdornment, InputBase } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
+import CompletionBox from './CompletionBox';
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
   color: 'inherit',
-	border: '2px solid #073113',
-	borderRadius: '2rem',
-	width: '50%',
-	height: '3rem',
+  border: '2px solid #073113',
+  borderRadius: '2rem',
+  width: '50%',
+  height: '3rem',
   '& .MuiInputBase-input': {
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-	}
+  },
 }));
 
 function SearchBar(props) {
-	const [searchData, setSearchData] = useState('');
+  const [searchWord, setSearchWord] = useState('');
+  const [searchGroupResult, setSearchGroupResult] = useState([]);
 
-	const onSearch = () => {
-		props.history.push(`/search/${searchData}`);
-	};
-	
-	const onChange = (e) => {
-		setSearchData(e.target.value);
-	}
-	
-	const onKeyPress = (e) => {
-    if( e.key === 'Enter' ) {
+  const onSearch = () => {
+    props.history.push(`/search/${searchWord}`);
+  };
+
+  const onChange = e => {
+    const {
+      target: { value },
+    } = e;
+    setSearchWord(value);
+    
+  };
+
+  const onKeyPress = e => {
+    if (e.key === 'Enter') {
       onSearch();
-      setSearchData('');
+      setSearchWord('');
     }
-	}
+  };
+  useEffect(() => {
+    if (searchWord === '') {
+      setSearchGroupResult([]);
+    } else {
+      Axios.post('/api/search/all', { search: searchWord }).then(response => {
+        if (response.data.success) {
+          setSearchGroupResult(response.data.Groups);
+        } else {
+          alert('Failed');
+        }
+      });
+    }
+  }, [searchWord]);
   return (
     <>
       <Box
         sx={{
           display: 'flex',
           justifyContent: 'center',
+          position: 'relative',
           borderBottom: '1px solid rgba(255, 255, 255, 0.15)',
           height: '4rem',
           '& input': {
@@ -50,28 +71,37 @@ function SearchBar(props) {
           placeholder="찾고 싶은 그룹을 검색해보세요."
           onChange={onChange}
           onKeyPress={onKeyPress}
-          value={searchData}
+          value={searchWord}
           endAdornment={
-            <InputAdornment
-              onClick={onSearch}
-              position="start"
-            >
+            <InputAdornment onClick={onSearch} position="start">
               <SearchIcon
                 sx={{
                   color: '#073113',
                   width: '2.5rem',
                   height: '2.5rem',
                   pl: 1,
-                  borderLeft:'2px solid #073113',
+                  borderLeft: '2px solid #073113',
                   cursor: 'pointer',
                 }}
               />
             </InputAdornment>
           }
         />
+        <Box
+          sx={{ position: 'absolute', left: '25%', top: '3rem', width: '50%' }}
+        >
+          {searchGroupResult.map(group => (
+            <CompletionBox
+              key={group._id}
+              content={group}
+              searchWord={searchWord}
+              setSearchWord={setSearchWord}
+            />
+          ))}
+        </Box>
       </Box>
     </>
-	);
+  );
 }
 
 export default withRouter(SearchBar);
